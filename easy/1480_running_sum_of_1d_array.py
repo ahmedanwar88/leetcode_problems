4e0493932a36a39924431cab96098b4c2621f644
Ahmed Anwar <ahmed.anwar.saeed@gmail.com>
2022-11-05 03:09:04 +0200

    Add problem solution

---
+++ easy/1480_running_sum_of_1d_array.py
@@ -0,0 +1,6 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        sums = []
+        for i in range(len(nums)):
+            sums.append(sum(nums[0:i+1]))
+        return sums
